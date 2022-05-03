9634cb3e96a45cdd008dea0d8dfb488507a42177
Younglong Kim <withel1013@gmail.com>
2022-05-03 20:30:30 +0900

    Initialization

--- App.js
+++ App.js
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+/* AppLoading: Keep the splash screen visible while the AppLoading component is mounted (cache fonts, logos, icon images, etc.) */
+import AppLoading from 'expo-app-loading';
+import { View, Text } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [ready, setReady] = useState(false);
+  const onFinish = () => setReady(true);
+  const startLoading = async () => {
+    /* await new Promise((resolve) => setTimeout(resolve, 5000)); */
+  }; /* Every 'await' thing comes here */
+  
+  if (!ready) {
+    return <AppLoading startAsync={startLoading} onFinish={onFinish} onError={console.warn} />;
+  }
+  
+  return <Text>Hello!</Text>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
